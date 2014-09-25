4a48759f33ef1d4bc706e6c829f0beed2ac851a1
Cyril Fougeray <cyril.fougeray@gmail.com>
2014-09-25 20:38:41 +0200

    First commit : FXAS21000 sensor implemented with basic methods

---
+++ Beans/FXAS21000/FXAS21000.tps
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TypesAndGlobals>
+  <Types>
+    <Type>
+      <Type>TEnumSpec</Type>
+      <Name>typePropertyI2CSlaveAddress</Name>
+      <Items lines_count="2">
+        <Line>0x20</Line>
+        <Line>0x21</Line>
+      </Items>
+      <Hints lines_count="2">
+        <Line>Address 0x20, with SA0 (pin 13) set to 0.</Line>
+        <Line>Address 0x21, with SA0 (pin 13) set to 1.</Line>
+      </Hints>
+      <Defines lines_count="2">
+        <Line>0x20</Line>
+        <Line>0x21</Line>
+      </Defines>
+    </Type>
+    <Type>
+      <Type>TEnumSpec</Type>
+      <Name>typePropertyODR</Name>
+      <Items lines_count="3">
+        <Line>100Hz</Line>
+        <Line>50Hz</Line>
+        <Line>25Hz</Line>
+      </Items>
+      <Hints lines_count="3">
+        <Line>Output Data Rate set to 100Hz (10ms)</Line>
+        <Line>Output Data Rate set to 50Hz (20ms)</Line>
+        <Line>Output Data Rate set to 25Hz (40ms)</Line>
+      </Hints>
+      <Defines lines_count="3">
+        <Line>100</Line>
+        <Line>50</Line>
+        <Line>25</Line>
+      </Defines>
+    </Type>
+  </Types>
+</TypesAndGlobals>
+
---
+++ Sources/Events.h
@@ -0,0 +1,145 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : FXAS21000
+**     Processor   : MKL26Z128VMC4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-09-15, 22:18, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "HF1.h"
+#include "FRTOS1.h"
+#include "FX1.h"
+#include "TMOUT1.h"
+#include "CS1.h"
+#include "I2C0.h"
+#include "GI2C0.h"
+#include "LP1.h"
+#include "WAIT1.h"
+#include "UTIL1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL26Z256MC4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+void FRTOS1_vApplicationStackOverflowHook(xTaskHandle pxTask, char *pcTaskName);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationStackOverflowHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         if enabled, this hook will be called in case of a stack
+**         overflow.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         pxTask          - Task handle
+**       * pcTaskName      - Pointer to task name
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void FRTOS1_vApplicationTickHook(void);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationTickHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called by the RTOS for every
+**         tick increment.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void FRTOS1_vApplicationIdleHook(void);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationIdleHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, this hook will be called when the RTOS is idle.
+**         This might be a good place to go into low power mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+void FRTOS1_vApplicationMallocFailedHook(void);
+/*
+** ===================================================================
+**     Event       :  FRTOS1_vApplicationMallocFailedHook (module Events)
+**
+**     Component   :  FRTOS1 [FreeRTOS]
+**     Description :
+**         If enabled, the RTOS will call this hook in case memory
+**         allocation failed.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
